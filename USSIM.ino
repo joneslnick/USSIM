8b95ab951f14bc0326a1167756a59aa26e865893
Nick Jones <njones9@umbc.edu>
2023-05-10 09:12:49 -0400

    Updated table generation, timer setup

--- USSIM.ino
+++ USSIM.ino
@@ -9,6 +9,9 @@ Group 01
 void setup() {
   // put your setup code here, to run once:
 
+  setup_timer();
+  setup_dac();
+
   // Setup Serial
   Serial.begin(BAUD_RATE);
 
@@ -42,8 +45,14 @@ void read_tcu_serial() {
   // Shift left once for each read
   SERIAL_IN = SERIAL_IN << 1;
 
+  int status = digitalRead(TCU_IN);
   // Add new read to result, equivalent of setting last bit to 0 or 1
-  SERIAL_IN = SERIAL_IN + digitalRead(TCU_IN);
+  SERIAL_IN = SERIAL_IN + status;
+
+  char print_out[20];
+  sprintf(print_out, "Received: %d\n", status);
+  //Serial.write(print_out);
+
   return;
 }
 
@@ -66,7 +75,7 @@ void implement_func() {
   // DECODE
 
   Serial.write("\tTRANSMITTER: ");
-  if (SERIAL_IN & 1000000) {
+  if (SERIAL_IN & 0b01000000) {
     Serial.write("ON\n");
   }
   else {
@@ -177,8 +186,78 @@ void setup_dac() {
 Configures TC5
 */
 void setup_timer() {
+  // select the generic clock generator used as source to the generic clock multiplexer
+  GCLK->CLKCTRL.reg = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK0 | GCLK_CLKCTRL_ID(GCM_TC4_TC5)) ;
+  while (GCLK->STATUS.bit.SYNCBUSY);
+
+  tcReset(); //reset TC5
+
+  // Set Timer counter 5 Mode to 16 bits, it will become a 16bit counter ('mode1' in the datasheet)
+  TC5->COUNT16.CTRLA.reg |= TC_CTRLA_MODE_COUNT16;
+  // Set TC5 waveform generation mode to 'match frequency'
+  TC5->COUNT16.CTRLA.reg |= TC_CTRLA_WAVEGEN_MFRQ;
+  //set prescaler
+  //the clock normally counts at the GCLK_TC frequency, but we can set it to divide that frequency to slow it down
+  //you can use different prescaler divisons here like TC_CTRLA_PRESCALER_DIV1 to get a different range
+  TC5->COUNT16.CTRLA.reg |= TC_CTRLA_PRESCALER_DIV1 | TC_CTRLA_ENABLE; //it will divide GCLK_TC frequency by 1
+  //set the compare-capture register. 
+  //The counter will count up to this value (it's a 16bit counter so we use uint16_t)
+  //this is how we fine-tune the frequency, make it count to a lower or higher value
+  //F_CPU should be 48 MHZ
+  // Trigger every 1 us
+  TC5->COUNT16.CC[0].reg = (uint16_t) (F_CPU / 1e6 - 1);
+  while (tcIsSyncing());
+  
+  // Configure interrupt request
+  NVIC_DisableIRQ(TC5_IRQn);
+  NVIC_ClearPendingIRQ(TC5_IRQn);
+  NVIC_SetPriority(TC5_IRQn, 0);
+  NVIC_EnableIRQ(TC5_IRQn);
+
+  // Enable the TC5 interrupt request
+  TC5->COUNT16.INTENSET.bit.MC0 = 1;
+  while (tcIsSyncing()); //wait until TC5 is done syncing 
 }
 
+//Function that is used to check if TC5 is done syncing
+//returns true when it is done syncing
+bool tcIsSyncing()
+{
+  return TC5->COUNT16.STATUS.reg & TC_STATUS_SYNCBUSY;
+}
+
+//This function enables TC5 and waits for it to be ready
+void tcStartCounter()
+{
+  TC5->COUNT16.CTRLA.reg |= TC_CTRLA_ENABLE; //set the CTRLA register
+  while (tcIsSyncing()); //wait until sync'd
+}
+
+//Reset TC5 
+void tcReset()
+{
+  TC5->COUNT16.CTRLA.reg = TC_CTRLA_SWRST;
+  while (tcIsSyncing());
+  while (TC5->COUNT16.CTRLA.bit.SWRST);
+}
+
+//disable TC5
+void tcDisable()
+{
+  TC5->COUNT16.CTRLA.reg &= ~TC_CTRLA_ENABLE;
+  while (tcIsSyncing());
+}
+
+
+/* 
+TC5 Interrupt Handler
+*/
+void TC5_Handler (void) {
+  //YOUR CODE HERE 
+
+  // END OF YOUR CODE
+  TC5->COUNT16.INTFLAG.bit.MC0 = 1; //Writing a 1 to INTFLAG.bit.MC0 clears the interrupt so that it will run again
+}
 
 /*
 Sets up and executes a scanning beam simulation
